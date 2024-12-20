4b7e5826c83c67880747dce4cba4e8d715746e8b
Brett C. Dudo <brett@dudo.io>
2024-12-20 15:49:38 -0800

    terminal mw

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ import express from "express";
 
 import routes from "./routes/index.js";
 import middleware from "./middleware/index.js";
+import terminalMiddleware from "./middleware/terminalMiddleware.js";
+
 
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Loop through all middleware
+// Loop through all general middleware
 middleware.forEach((mw) => app.use(mw));
 
 // Primary application routes
 app.use("/api", routes);
+
+// Terminal middleware to be called at the end of the stack
+terminalMiddleware.forEach((mw) => app.use(mw));
 
 export default app;
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,5 @@
 import { loggingHandler } from "./loggingMiddleware.js";
-import { notFoundHandler } from "./notFoundMiddleware.js";
-import { errorHandler } from "./errorMiddleware.js";
 
 export default [
   loggingHandler,
-  notFoundHandler,
-  errorHandler,
 ];
