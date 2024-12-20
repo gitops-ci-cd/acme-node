9c46c1da097e90c0d3d874c591eefb725c698418
Brett C. Dudo <brett@dudo.io>
2024-12-20 15:19:48 -0800

    save the normalization

--- src/controllers/v1/greetingController.js
+++ src/controllers/v1/greetingController.js
@@ -4,7 +4,7 @@ import { fetchPerson } from "../../clients/personClient.js";
 export const greet = async (req, res, next) => {
   try {
     let acceptedLanguages = req.headers["accept-language"]?.split(",") || [];
-    acceptedLanguages.map((lang) => lang.split(";")[0].trim().toUpperCase()); // Normalize languages
+    acceptedLanguages = acceptedLanguages.map((lang) => lang.split(";")[0].trim().toUpperCase()); // Normalize languages
 
     const id = req.query.personID || req.params.personID;
 
