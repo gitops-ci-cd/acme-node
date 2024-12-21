f3f7cfecd982ca4dcbbf34c3976e116a922a8cab
Brett C. Dudo <brett@dudo.io>
2024-12-20 18:30:44 -0800

    small refactor

--- src/middleware/loggingMiddleware.js
+++ src/middleware/loggingMiddleware.js
@@ -1,33 +1,32 @@
 export const loggingMiddleware = (req, res, next) => {
   const start = Date.now();
 
+  const base = {
+    timestamp: new Date().toISOString(),
+    method: req.method,
+    path: `${req.baseUrl}${req.path}`,
+    params: req.query || req.params,
+  };
+
   // Hook into the response lifecycle to log when it finishes
   res.on("finish", () => {
     const duration = Date.now() - start;
     const log = {
-      timestamp: new Date().toISOString(),
-      method: req.method,
-      path: `${req.baseUrl}${req.path}`,
-      params: req.query || req.params,
       status: res.statusCode,
       duration: `${duration}ms`,
     };
-    console.info(JSON.stringify(log));
+    console.info(JSON.stringify({ ...base, ...log }));
   });
 
   // Hook into the response lifecycle to log errors
   res.on("error", (err) => {
     const duration = Date.now() - start;
     const log = {
-      timestamp: new Date().toISOString(),
-      method: req.method,
-      path: `${req.baseUrl}${req.path}`,
-      params: req.query || req.params,
-      status: res.statusCode,
+      status: res.statusCode || 500,
       duration: `${duration}ms`,
       error: err.message,
     };
-    console.error(JSON.stringify(log));
+    console.error(JSON.stringify({ ...base, ...log }));
   });
 
   next();
