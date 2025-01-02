8bbc164bfb719eec3cf41aa4d6cc82fdf80f1077
Brett C. Dudo <brett@dudo.io>
2025-01-01 18:40:47 -0800

    otel textMapPropagator

--- src/lib/instrumentation.js
+++ src/lib/instrumentation.js
@@ -1,10 +1,12 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { W3CBaggagePropagator } from "@opentelemetry/core";
 
 const sdk = new NodeSDK({
   traceExporter: new ConsoleSpanExporter(),
   instrumentations: [getNodeAutoInstrumentations()],
+  textMapPropagator: new W3CBaggagePropagator(),
 });
 
 sdk.start();
