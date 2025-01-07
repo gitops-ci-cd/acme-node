31cafd3bf675c83d952c068078a9c6009604b7f4
Brett C. Dudo <brett@dudo.io>
2025-01-07 11:33:24 -0800

    remove explicit context

--- src/controllers/v1/greetingController.js
+++ src/controllers/v1/greetingController.js
@@ -1,24 +1,18 @@
-import { context } from "@opentelemetry/api";
-
 import { fetchGreetingAsync, detectLanguage } from "../../clients/greetingClient.js";
 import { fetchPersonAsync } from "../../clients/personClient.js";
 
 export const greet = async (req, res, next) => {
   try {
-    const activeContext = context.active();
     let acceptedLanguages = req.headers["accept-language"]?.split(",") || [];
     acceptedLanguages = acceptedLanguages.map((lang) => lang.split(";")[0].trim().toUpperCase().replace(/-/g, "_")); // Normalize languages
 
     const language = detectLanguage(acceptedLanguages);
     const uuid = req.query.personID || req.params.personID;
 
-    // Use context.with() to ensure the trace context is propagated
-    const [greetingResponse, personResponse] = await context.with(activeContext, () =>
-      Promise.all([
-        fetchGreetingAsync({ language }),
-        fetchPersonAsync({ uuid }),
-      ])
-    );
+    const [greetingResponse, personResponse] = await Promise.all([
+      fetchGreetingAsync({ language }),
+      fetchPersonAsync({ uuid }),
+    ]);
 
     res.status(200).json({
       data: {
