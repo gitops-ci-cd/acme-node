b99302b2f5f046d9bf26728b09f2ee67e8be53bb
lephuocthanh2000 <lephuocthanhnbk10h@gmail.com>
2025-06-26 13:30:58 +0700

    feat(hello) update controller hello12

--- src/routes/v1/greetingRoutes.js
+++ src/routes/v1/greetingRoutes.js
@@ -4,7 +4,7 @@ import { greet } from "../../controllers/v1/greetingController.js";
 
 const router = Router();
 
-router.get("/hello", greet);
+router.get("/hello12, greet);
 
 
 export default router;
